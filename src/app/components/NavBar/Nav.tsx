5ed2dd077999ca08590aea6f672f8f601b3d4fc3
Ognjen Popovic <ognjen.popovic.tt@gmail.com>
2021-11-14 18:31:33 +0100

    typescript unused variables

--- src/app/components/NavBar/Nav.tsx
+++ src/app/components/NavBar/Nav.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components/macro';
-import { ReactComponent as GithubIcon } from './assets/github-icon.svg';
 
 export function Nav() {
   return (
@@ -16,25 +15,25 @@ const Wrapper = styled.nav`
   margin-right: -1rem;
 `;
 
-const Item = styled.a`
-  color: ${p => p.theme.primary};
-  cursor: pointer;
-  text-decoration: none;
-  display: flex;
-  padding: 0.25rem 1rem;
-  font-size: 0.875rem;
-  font-weight: 500;
-  align-items: center;
+// const Item = styled.a`
+//   color: ${p => p.theme.primary};
+//   cursor: pointer;
+//   text-decoration: none;
+//   display: flex;
+//   padding: 0.25rem 1rem;
+//   font-size: 0.875rem;
+//   font-weight: 500;
+//   align-items: center;
 
-  &:hover {
-    opacity: 0.8;
-  }
+//   &:hover {
+//     opacity: 0.8;
+//   }
 
-  &:active {
-    opacity: 0.4;
-  }
+//   &:active {
+//     opacity: 0.4;
+//   }
 
-  .icon {
-    margin-right: 0.25rem;
-  }
-`;
+//   .icon {
+//     margin-right: 0.25rem;
+//   }
+// `;
